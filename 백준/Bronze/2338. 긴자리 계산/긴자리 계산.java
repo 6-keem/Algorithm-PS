f699a89c1952d4f7ef6020486049b05d848c115e
Keem <113224939+u6-k@users.noreply.github.com>
2024-01-28 23:28:15 +0900

    [Bronze V] Title: 긴자리 계산, Time: 144 ms, Memory: 15212 KB -BaekjoonHub

---
+++ 백준/Bronze/2338. 긴자리 계산/긴자리 계산.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BigInteger a = new BigInteger(bufferedReader.readLine());
+        BigInteger b = new BigInteger(bufferedReader.readLine());
+
+        System.out.println(a.add(b));
+        System.out.println(a.subtract(b));
+        System.out.println(a.multiply(b));
+    }
+}
